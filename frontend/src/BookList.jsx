1d307e5e74eac2b719d0497c5f4a0165adbcff53
carolhuy <68884821+carolhuy@users.noreply.github.com>
2025-01-29 17:23:54 -0800

    add book functionality

--- frontend/src/BookList.jsx
+++ frontend/src/BookList.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import BookCard from "./components/BookCard";
 import Grid from '@mui/joy/Grid'
+import Button from '@mui/joy/Button';
 
 export default function BookList({books}){
     return (
         <>
             <h1>Books</h1>
-            <Grid container direction="row" sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              minHeight: 180,
-            }}>
+            <Button>+ Add Book</Button>
+            <Grid container direction="row" spacing={3}>
                 {books.map( b => {
-                return <BookCard key = {b.id} book={{title:b.title, author:b.author, genre:b.genre}}/>
+                return <Grid size={12}><BookCard key = {b.id} book={{title:b.title, author:b.author, genre:b.genre}}/></Grid>
                 })}
             </Grid>
         </>
